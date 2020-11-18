77c82f30cd9230477987c7f46e044c996e6ce28f
jhersheyy <jhersheyy@users.noreply.github.com>
2020-11-17 22:47:42 -0800

    edited index page to include about section

---
+++ src/client/other.js
@@ -0,0 +1,17 @@
+import logo from './pics/jh.png';
+
+// import { checkURL } from './js/urlChecker'
+// import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/base.scss'
+// import './styles/footer.scss'
+// import './styles/form.scss'
+import './styles/header.scss'
+var mylogo = document.getElementById('jhlogo');
+mylogo.src = logo;
+
+// export {
+    // checkURL,
+    // handleSubmit,
+// 
+// }
